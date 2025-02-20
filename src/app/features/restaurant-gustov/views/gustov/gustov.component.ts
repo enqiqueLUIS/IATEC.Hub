f4787b4a727093e2739204740c150bbcfe0a1cd3
enqiqueLUIS <luis.luna@uab.edu.bo>
2025-02-20 02:09:21 -0400

    feat(create): create sale

--- src/app/features/restaurant-gustov/views/gustov/gustov.component.ts
+++ src/app/features/restaurant-gustov/views/gustov/gustov.component.ts
@@ -4,34 +4,43 @@ import { RouterModule } from '@angular/router';
 import { DishComponent } from '../../controls/dish/dish.component';
 import { PaymentMethodsComponent } from '../../controls/payment-methods/payment-methods.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SaleComponent } from '../../controls/sale-dashboard/sale/sale.component';
 
 @Component({
   selector: 'features-gustov',
   standalone: true,
-  imports: [RouterModule, CommonModule, DishComponent, HttpClientModule, PaymentMethodsComponent],
+  imports: [RouterModule, CommonModule, DishComponent, HttpClientModule, PaymentMethodsComponent, SaleComponent],
   templateUrl: './gustov.component.html',
 })
-export default class GustovComponent  {
+export default class GustovComponent {
   isMenuOpen = false;
   isDishesVisible = false;
   isPaymentMethodsVisible = false;
+  isSaleVisible = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
   toggleDishes() {
-    this.isDishesVisible = !this.isDishesVisible;
-    if (this.isDishesVisible) {
-      this.isPaymentMethodsVisible = false;
-    }
+    const wasVisible = this.isDishesVisible;
+    this.isDishesVisible = !wasVisible;
+    this.isPaymentMethodsVisible = false;
+    this.isSaleVisible = false;
   }
 
   togglePaymentMethods() {
-    this.isPaymentMethodsVisible = !this.isPaymentMethodsVisible;
-    if (this.isPaymentMethodsVisible) {
-      this.isDishesVisible = false;
-    }
+    const wasVisible = this.isPaymentMethodsVisible;
+    this.isPaymentMethodsVisible = !wasVisible;
+    this.isDishesVisible = false;
+    this.isSaleVisible = false;
   }
 
+  toggleSale() {
+    const wasVisible = this.isSaleVisible;
+    this.isSaleVisible = !wasVisible;
+    this.isDishesVisible = false;
+    this.isPaymentMethodsVisible = false;
+  }
 }
+
